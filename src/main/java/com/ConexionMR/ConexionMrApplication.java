712d4d478d686ee991f9f7c8655df15fecf8ad19
Alvaro Rios <alvaroluisriosgarzon@gmail.com>
2021-12-09 11:03:21 -0500

    Update

--- src/main/java/com/ConexionMR/ConexionMrApplication.java
+++ src/main/java/com/ConexionMR/ConexionMrApplication.java
@@ -1,4 +1,4 @@
-package com.ConexionMR;
+package com.conexionMR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ConexionMR/ConexionMrApplicationTests.java
+++ src/test/java/com/ConexionMR/ConexionMrApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ConexionMR;
+package com.conexionMR;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
